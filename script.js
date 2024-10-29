955a60d46541a63d932bea76c4e3e933d2d44be5
matheus <matheusmoreirarocha@gmail.com>
2024-10-29 16:27:03 -0300

    tirei telefone

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function fetchData() {
       const rowElement = document.createElement('tr');
 
       // Criação das células para cada campo
-      ['ID', 'Nome', 'Cargo', 'Telefone', 'Estado Civil', 'Ingresso', 'Niver', 'FP', 'FC', 'Quant de formandos', 'Disponível para novos formandos?'].forEach(field => {
+      ['ID', 'Nome', 'Cargo', 'Estado Civil', 'Ingresso', 'Niver', 'FP', 'FC', 'Quant de formandos', 'Disponível para novos formandos?'].forEach(field => {
         const cellElement = document.createElement('td');
         cellElement.textContent = row[field] || '';
         rowElement.appendChild(cellElement);
